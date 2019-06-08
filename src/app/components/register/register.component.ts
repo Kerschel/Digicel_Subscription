8c9885dc2598fdf2e9092bf92569089ff7de2a66
Kerschel James <kerschels@hotmail.com>
2019-06-08 07:39:53 -0400

    added sweet alerts to register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService, TokenPayload } from '../../authentication.service';
 import { Router } from '@angular/router'
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -17,11 +17,16 @@ export class RegisterComponent implements OnInit {
 constructor(private auth: AuthenticationService, private router: Router){}
 
   ngOnInit() {
-
+    Swal.fire(
+      'Registered!',
+      'Account created',
+      'success'
+    )
   }
   register () {
     // register only if user entered username and password
       if(this.credentials.username.length>0 && this.credentials.password.length>0){
+        
         this.auth.register(this.credentials).subscribe(
           () => {
               this.router.navigateByUrl('/login')
